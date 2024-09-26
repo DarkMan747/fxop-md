4bb76733d8b0fa963a33a2b1614b7a0e8fca9c9e
FX <danielfx011@gmail.com>
2024-09-26 20:36:42 +0100

    fix: sleeps & plugins

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const config = require("./config");
-const { requireJS, retrivePlugins, client } = require("./lib");
+const { requireJS, retrivePlugins, client, sleep } = require("./lib");
 async function initialize() {
  await requireJS(path.join(__dirname, "/lib/Client/Stores/"));
  console.log("Syncing Database");
+ await sleep(3000);
  await config.DATABASE.sync();
- console.log("⬇  Installing Plugins...");
+ console.log("⬇ Modules Installed");
  await requireJS(path.join(__dirname, "/plugins/"));
  await retrivePlugins();
  return await client();
