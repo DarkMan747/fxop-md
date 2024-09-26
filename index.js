8e4db74baa2519fa7896856c9f78b8256caf0d2d
FX <danielfx011@gmail.com>
2024-09-26 18:02:33 +0100

    fix

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const { promises: fs } = require("fs");
-const path = require("path");
-const config = require("./config");
-const { retrivePlugins, client } = require("./lib");
-const requireJS = async (dir, { recursive = false, fileFilter = f => path.extname(f) === ".js" } = {}) => {
- const files = recursive ? (await Promise.all((await fs.readdir(dir, { withFileTypes: true })).map(e => path.resolve(dir, e.name)).map(async p => ((await fs.stat(p)).isDirectory() ? requireJS(p, { recursive, fileFilter }) : p)))).flat() : await fs.readdir(dir);
-
- return Promise.all(
-  files.filter(fileFilter).map(f => {
-   const filePath = path.isAbsolute(f) ? f : path.join(dir, f);
-   try {
-    return require(filePath);
-   } catch (err) {
-    return null;
-   }
-  })
- ).then(m => m.filter(Boolean));
-};
+const path = require('path');
+const config = require('./config');
+const { requireJS, retrivePlugins, client } = require('./lib');
 async function initialize() {
- await requireJS(path.join(__dirname, "/lib/Client/Stores/"));
- console.log("Syncing Database");
+ await requireJS(path.join(__dirname, '/lib/Client/Stores/'));
+ console.log('Syncing Database');
  await config.DATABASE.sync();
- console.log("⬇  Installing Plugins...");
- await requireJS(path.join(__dirname, "/plugins/"));
+ console.log('⬇  Installing Plugins...');
+ await requireJS(path.join(__dirname, '/plugins/'));
  await retrivePlugins();
  return await client();
 }
