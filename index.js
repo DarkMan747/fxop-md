9ad5105c11ddb89d0b8c4201b64206a07fcb4f90
FX <danielfx011@gmail.com>
2024-09-27 13:37:13 +0100

    fix: version

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const { requireJS, retrivePlugins, client } = require("./lib");
 const config = require("./config");
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Bot connected" }));
-
-module.exports = app.listen(8000, async () => {
+app.get("/", (req, res) => res.json({ version: require("./package.json").version }));
+app.listen(8000, async () => {
  (await new Promise(res => setTimeout(res, 2500))) && config.DATABASE.sync();
  (await requireJS(join(__dirname, "./lib/Client/Stores/"))) && (await requireJS(join(__dirname, "./plugins/")));
  await retrivePlugins();
  return await client();
 });
-app();
