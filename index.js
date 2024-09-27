d90e9ad1cc6b68c6e5285aa54416516550987d47
FX <danielfx011@gmail.com>
2024-09-27 13:16:24 +0100

    fix: runs

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const { requireJS, retrivePlugins, client, sleep } = require("./lib");
+const { requireJS, retrivePlugins, client } = require("./lib");
 const config = require("./config");
 const app = express();
 
 app.get("/", (req, res) => res.json({ message: "Bot connected" }));
 
 module.exports = app.listen(8000, async () => {
- await requireJS("./lib/Client/Stores/");
- await sleep(3000);
- await config.DATABASE.sync();
- await requireJS("./plugins/");
+ (await new Promise(res => setTimeout(res, 1000))) && config.DATABASE.sync();
+ (await requireJS("./lib/Client/Stores/")) && (await requireJS("./plugins/"));
  await retrivePlugins();
- await client();
+ return await client();
 });
 app();
