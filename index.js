a36aae9024a2cbb49c41d121c267aed5e9703a75
FX <danielfx011@gmail.com>
2024-09-27 13:35:36 +0100

    fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
+const { join } = require("path");
 const { requireJS, retrivePlugins, client } = require("./lib");
 const config = require("./config");
 const app = express();
 
-app.get("/", (req, res) => res.json({ version: `${require("./package.json").version}` }));
+app.get("/", (req, res) => res.json({ message: "Bot connected" }));
 
-app.listen(8000, async () => {
- await new Promise(res => setTimeout(res, 2500));
- await config.DATABASE.sync();
- await requireJS("./lib/Client/Stores/");
- await requireJS("./plugins/");
+module.exports = app.listen(8000, async () => {
+ (await new Promise(res => setTimeout(res, 2500))) && config.DATABASE.sync();
+ (await requireJS(join(__dirname, "./lib/Client/Stores/"))) && (await requireJS(join(__dirname, "./plugins/")));
  await retrivePlugins();
- await client();
+ return await client();
 });
+app();
