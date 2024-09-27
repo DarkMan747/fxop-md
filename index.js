4148b8af107e38f14c746f0f7a41d747d7ead73d
FX <danielfx011@gmail.com>
2024-09-27 18:46:17 +0100

    fix

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.get("/", (req, res) => res.json({ version: require("./package.json").version }));
 app.listen(8000, async () => {
- (await new Promise(res => setTimeout(res, 2500))) && config.DATABASE.sync();
- (await requireJS(join(__dirname, "./lib/Client/Stores/"))) && (await requireJS(join(__dirname, "./plugins/"))) && (await retrivePlugins());
+ await config.DATABASE.sync();
+ (await requireJS(join(__dirname, "./lib/Client/Stores/"))) && (await requireJS(join(__dirname, "./plugins/")));
+ await retrivePlugins();
  return await client();
 });
