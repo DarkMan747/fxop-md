b98a6f815dace11719c6d98b0fc33584df35ccb7
FX <danielfx011@gmail.com>
2024-09-27 13:20:52 +0100

    fix: run node express

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const { requireJS, retrivePlugins, client } = require("./lib");
 const config = require("./config");
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Bot connected" }));
-
-module.exports = app.listen(8000, async () => {
- (await new Promise(res => setTimeout(res, 2500))) && config.DATABASE.sync();
- (await requireJS("./lib/Client/Stores/")) && (await requireJS("./plugins/"));
- await retrivePlugins();
+app.get("/", (req, res) => res.json({ message: `${require("./package.json").version}` }));
+app.listen(8000, async () => {
+ (await new Promise(res => setTimeout(res, 2500))) && (await config.DATABASE.sync());
+ (await requireJS("./lib/Client/Stores/")) && (await requireJS("./plugins/")) && (await retrivePlugins());
  return await client();
 });
-app();
