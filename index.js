f15332f3f03c040f934c4d7229d404fc15d4cf65
FX <danielfx011@gmail.com>
2024-09-27 13:16:47 +0100

    fix: timeouts patch db sync

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.get("/", (req, res) => res.json({ message: "Bot connected" }));
 
 module.exports = app.listen(8000, async () => {
- (await new Promise(res => setTimeout(res, 1000))) && config.DATABASE.sync();
+ (await new Promise(res => setTimeout(res, 2500))) && config.DATABASE.sync();
  (await requireJS("./lib/Client/Stores/")) && (await requireJS("./plugins/"));
  await retrivePlugins();
  return await client();
